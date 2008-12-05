e1e03ee4e70f74e4c889a3459a3ca0ff38594970
raymooz <raymooz@e8300490-ab3b-11dd-a772-7fcde58d38e6>
2008-12-05 16:19:11 +0000

    Added the model parser

---
+++ trunk/ModelParser/modelParser/rootManager/NodeType.java
@@ -0,0 +1,11 @@
+package modelParser.rootManager;
+
+public class NodeType {
+
+    public static int PLACE = 1;
+    public static int TRANS = 2;
+    public static int SYNC = 3;
+    public static int CHANNEL = 4;
+    public static int NAN = 5;
+
+}

7f7e82577300f2fe4a788866dae37e280a5536cd
florent.weber <florent.weber@e8300490-ab3b-11dd-a772-7fcde58d38e6>
2008-12-06 14:12:21 +0000

    Cleaned generated source code, and added some relevant logging.

--- trunk/RAAR-env-projet/runtime.c
+++ trunk/RAAR-env-projet/runtime.c
@@ -1,28 +1,26 @@
 /*
-##=======================================================================
-##Copyright (C) 2007-2008 C_MPI Generator
-##This program is free software; you can redistribute it and/or modify
-##it under the terms of the GNU General Public License (GPL) as published
-##of the License, or (at your option) any later version.
-##
-##This program is distributed in the hope that it will be useful,
-##but WITHOUT ANY WARRANTY; without even the implied warranty of
-##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-##GNU General Public License for more details.
-##
-##To read the license please visit http://www.gnu.org/copyleft/gpl.html
-##=======================================================================
-##
-## File          : hello.c
-## Authors       : Leonardo BAUTISTA GOMEZ <leobago@gmail.com>
-##               : Florent WEBER <florent.weber@gmail.com>
-##               : Julien CLEMENT <julien.jclement@gmail.com>
-##               : Felix MACH <felix.mach@gmail.com>
-##               : Henry LAY <lay.henri83@gmail.com>
-##               : Remi VILLE <raymooz@gmail.com>
-##               : Shi-Hon CHAN <syone7@gmail.com>
-##
-##
+	=======================================================================
+	Copyright (C) 2007-2008 C_MPI Generator
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License (GPL) as published
+	of the License, or (at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	To read the license please visit http://www.gnu.org/copyleft/gpl.html
+	=======================================================================
+	
+	File			: runtime.c
+	Authors			: Leonardo BAUTISTA GOMEZ <leobago@gmail.com>
+					: Florent WEBER <florent.weber@gmail.com>
+					: Julien CLEMENT <julien.jclement@gmail.com>
+					: Felix MACH <felix.mach@gmail.com>
+					: Henry LAY <lay.henri83@gmail.com>
+					: Remi VILLE <raymooz@gmail.com>
+					: Shi-Hon CHAN <syone7@gmail.com>
 */
 
 #include <math.h>
